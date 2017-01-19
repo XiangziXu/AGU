e13298d9b88fdc635879dc1a75e5ba0e024ca036
xzx <2082733106@qq.com>
2017-01-19 20:59:59 +0800

    XiangziXu，2017/1/19，第一次测试

--- js.js
+++ js.js
@@ -1,16 +1,29 @@
 function func()
 {
-	alert("哼我就不打你");
+	var a = Math.floor(Math.random() * 10);
+	alert("恭喜你发现了你的幸运数字，这个数字是" + a);
 }
 function func1()
 {
-	alert(NaN == NaN);
+	alert("儿子真乖");
+	alert("快叫爸爸");
+	alert("不叫爸爸是吧");
+	alert("算了我也不是那么绝的人");
+	alert("你想出去吗");
+	alert("我就放你出去吧");
+	alert("出去了记得好好做人");
+	alert("不要再来看这种奇奇怪怪的网页");
+	alert("再见");
+	alert("记得，我叫翔子");
 }
 function func2()
 {
-	alert("祝你们鸡年大吉吧");
+	alert("我也不知道该说什么了，给你们拜年又太早，给你们扫墓上香又太绝，祝你们鸡年大吉吧又不文明，干脆给各位买个棺材吧");
 }
 function func3()
 {
-	alert("此页面尚未开放 请稍等");
+	for(var b = 1;b >= 0;b++)
+	{
+		alert("我都叫你不要点了，上当了吧");
+	}
 }
